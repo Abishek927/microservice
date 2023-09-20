5ab49f7ef126f23779ba35e37ce9a19ca924e968
Abishek <khadkaabishek94@gmail.com>
2023-09-21 00:20:25 +0545

    implement spring security using okta in api gateway

--- UserService/UserService/src/main/java/com/abi/user/service/controller/UserController.java
+++ UserService/UserService/src/main/java/com/abi/user/service/controller/UserController.java
@@ -6,6 +6,8 @@ import com.abi.user.service.entities.User;
 import com.abi.user.service.proxy.HotelService;
 import com.abi.user.service.services.UserService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,21 +42,27 @@ public class UserController {
         User user1=userService.saveUser(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(user1);
     }
-    @GetMapping("/{userId}")
-    @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallBack")
-    public ResponseEntity<User> getSingleUser(@PathVariable String userId){
 
-        User user=userService.getUser(userId);
-        Ratings[] ratingsArray=restTemplate.getForObject(ratingUrl+user.getUserId(), Ratings[].class);//ArrayList cannot be mapped directly to rating array-list
-        List<Ratings> userRatings=Arrays.stream(ratingsArray).collect(Collectors.toList());
-        for (Ratings eachRating:userRatings
-             ) {
+    int retryCount=1;
+    @GetMapping("/{userId}")
+    //@Retry(name="ratingHotelService",fallbackMethod = "ratingHotelFallBack")
+   // @CircuitBreaker(name="ratingHotelBreaker",fallbackMethod = "ratingHotelFallBack")
+    @RateLimiter(name="userRateLimiter",fallbackMethod = "ratingHotelFallBack")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        logger.info("Get single user handler: User controller");
+        logger.info("Retry count:{}",retryCount);
+        retryCount++;
+        User user = userService.getUser(userId);
+        Ratings[] ratingsArray = restTemplate.getForObject(ratingUrl + user.getUserId(), Ratings[].class);//ArrayList cannot be mapped directly to rating array-list
+        List<Ratings> userRatings = Arrays.stream(ratingsArray).collect(Collectors.toList());
+        for (Ratings eachRating : userRatings
+        ) {
             //ResponseEntity<Hotel> forObject=restTemplate.getForEntity(hotelUrl+eachRating.getHotelId(),Hotel.class);
-            Hotel hotel=hotelService.getHotel(eachRating.getHotelId());
+            Hotel hotel = hotelService.getHotel(eachRating.getHotelId());
             eachRating.setHotel(hotel);
 
         }
-        logger.info("Rating for the user {}",userRatings);
+        logger.info("Rating for the user {}", userRatings);
         user.setRatings(userRatings);
 
         return ResponseEntity.ok(user);
@@ -62,6 +70,7 @@ public class UserController {
 
     //creating fall back method for circuitbreaker
     public ResponseEntity<User> ratingHotelFallBack(String userId,Exception ex){
+
         logger.info("Fallback is executed because service is down: ",ex.getMessage());
         User user=User.builder().email("dummy@gmail.com").name("Dummy").about("This is dummy user for some services down").build();
         return ResponseEntity.status(HttpStatus.OK).body(user);
